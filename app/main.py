fff34c1e8060e3921ea62b93bf11a0d638733606
Altyn-moon <altusshh.sss@gmail.com>
2025-04-17 14:52:44 +0500

    добавила интерфейс рабочего

--- app/main.py
+++ app/main.py
@@ -20,4 +20,17 @@ app.include_router(auth.router)
 
 #Base.metadata.create_all(bind=engine)
 
+@app.get("/dashboard", response_class=HTMLResponse)
+async def dashboard(request: Request):
+    user_name = "Иванов Иван"  # потом заменим на реального пользователя
+    work_items = [
+        {"id": 1, "name": "Изготовление переводника", "client": "ТОО «СБП» Казмунайгаз-Бурение", "job_num": "1135-24"},
+        {"id": 2, "name": "Втулка для шарнира", "client": "ТШО", "job_num": "1136-24"},
+    ]
+    return templates.TemplateResponse("dashboard.html", {
+        "request": request,
+        "user_name": user_name,
+        "work_items": work_items
+    })
+
 
